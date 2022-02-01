a45283c39992cbf2438470a93488ac45c094eebc
MonsterOnLeash <660867@mail.ru>
2022-02-01 05:04:12 +0300

    two rooms and cams

--- Bugometh/Assets/Scripts/Camera/CameraController.cs
+++ Bugometh/Assets/Scripts/Camera/CameraController.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CinemachineVirtualCamera target;
+    public CinemachineVirtualCamera selfCamera;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.CompareTag("Player"))
+        {
+            selfCamera.Priority = 0;
+            target.Priority = 10;
+        }
     }
 }
